466476bf85f697080a9dd16667c172264fed06d5
Ocal Kaptan <oecal.kaptan@tu-dortmund.de>
2022-07-14 19:27:44 +0200

    v1

--- Untitled.R
+++ Untitled.R
@@ -1,4 +1,20 @@
-library(ggplot2)
+library(parallel)
+
+#learning lapply 
+lapply(1:3, function(x) c(x,x^2,x^3))
+lapply(1:3/3,round, digits=3)
+#lapply take parameter(vector/list), feed the function and returns as a list
+
+nu_cores <- detectCores() -1 
+
+cl_1 <- makeCluster(nu_cores)
+cl_1
 
 
-print("This is a test")
+#Parallel version of lapply
+parLapply(cl_1,2:4,function(exponent) 2^exponent)
+lapply(2:4,function(exponent) 2^exponent)
+library(bench)
+bench::mark(parLapply(cl_1,2:4,function(exponent) 2^exponent),
+            lapply(2:4,function(exponent) 2^exponent))
+stopCluster(cl_1)
